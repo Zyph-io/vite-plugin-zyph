6e16f7f52bc2fe5ca6e871259ec8becadd624898
FlokiTV <dawysonw@gmail.com>
2024-02-17 20:40:18 -0300

    feat: init files

---
+++ index.js
@@ -0,0 +1,28 @@
+export default () => {
+  let config;
+  let mode;
+  return {
+    name: "my-plugin",
+    config(config, env) {
+      console.log("start", env);
+      mode = env.mode;
+    },
+    configResolved(resolvedConfig) {
+      config = resolvedConfig;
+    },
+    // https://vitejs.dev/guide/api-plugin#transformindexhtml
+    transformIndexHtml(html) {
+      if (mode === "production") return html;
+      return html.replace(
+        /<\/head>/,
+        `
+      <style>
+        @import url("./zyph/vars.css");
+      </style>
+      <script type="module" src="zyph"></script>
+    </head>
+      `
+      );
+    },
+  };
+};
